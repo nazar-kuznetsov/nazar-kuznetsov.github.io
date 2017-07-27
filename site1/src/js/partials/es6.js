bb96fef4dc953ab215e1c541113397da474c21fa
Nazar Kuznetsov <nazar.kuznettsov@gmail.com>
2017-07-27 12:10:54 +0300

    start

---
+++ site1/src/js/partials/es6.js
@@ -0,0 +1,4 @@
+let a1 = 'xxx'.includes('xxx'); // Ещет элемент в массиве и возращает true false
+let a2 = 'sad'.startsWith('sad'); // Проверяет начинаеться ли строка 
+let a3 = 'das'.endsWith('das'); // Проверяет заканчиваеться ли строка 
+
---
+++ site1/src/js/partials/preloader.js
@@ -0,0 +1,9 @@
+ document.body.onload = function() {
+    setTimeout(function () {
+      let preloader = document.getElementById('preloader');
+      if (!preloader.classList.contains('done')) {
+        preloader.classList.add('done');
+      }
+    }, 500);
+};
+
