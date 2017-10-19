0ced635e4cad948b46216c9f2f00483f5e43c486
Nazar Kuznetsov <nazar.kuznettsov@gmail.com>
2017-10-19 11:25:30 +0300

    'fix'

--- GeekHub-JS/1/js/main.js
+++ GeekHub-JS/1/js/main.js
@@ -1,2 +1,9 @@
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiJdLCJmaWxlIjoibWFpbi5qcyJ9
+document.body.onload = function () {
+  setTimeout(function () {
+    let preloader = document.getElementById('preloader');
+    if (!preloader.classList.contains('done')) {
+      preloader.classList.add('done');
+    }
+  }, 500);
+};
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImRvY3VtZW50LmJvZHkub25sb2FkID0gZnVuY3Rpb24gKCkge1xyXG4gIHNldFRpbWVvdXQoZnVuY3Rpb24gKCkge1xyXG4gICAgbGV0IHByZWxvYWRlciA9IGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdwcmVsb2FkZXInKTtcclxuICAgIGlmICghcHJlbG9hZGVyLmNsYXNzTGlzdC5jb250YWlucygnZG9uZScpKSB7XHJcbiAgICAgIHByZWxvYWRlci5jbGFzc0xpc3QuYWRkKCdkb25lJyk7XHJcbiAgICB9XHJcbiAgfSwgNTAwKTtcclxufTsiXSwiZmlsZSI6Im1haW4uanMifQ==
