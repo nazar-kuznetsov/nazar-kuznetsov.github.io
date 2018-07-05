108fa9074d9a2b6824323c48b98a1111b33bba07
Nazar Kuznetsov <nazar.kuznettsov@gmail.com>
2018-07-05 18:38:26 +0300

    'slider'

--- fast/common.js
+++ fast/common.js
@@ -1,30 +1,48 @@
 var li = document.querySelectorAll('.slider__item');
+li[0].style.maxWidth = '100%';
 var count = 0;
+var flag = false;
 
 document.querySelector('#next').addEventListener('click', function () {
-  if (count === 3) {
-    count = 0;
-    li.forEach(function(element, i) {
+  if (flag) return;
+  count += 1;
+  flag = true;
+  if (count === 2) {
+    li[0].style.maxWidth = 0;
 
-      element.style.zIndex = i;
-      element.style.maxWidth = 0;
-    })
+  }
+
+  if (count === 3) {
+
+    count = 0;
+    for (let i = 0; i < li.length; i++) {
+      li[i].style.zIndex = -1;
+    }
+
+
+    setTimeout(() => {
+    for (let i = 1; i < li.length; i++) {
+      li[i].style.maxWidth = 0;
+    }
+    }, 500);
+
 
   }
 
 
-  li[count].style.maxWidth = '400px';
-  
+
+  li[count].style.maxWidth = '100%';
+
   li[count].style.zIndex = count;
-  
 
-  count += 1;
-  
- // li[count].style.maxWidth = '100%';
-});
+  li[count].addEventListener("transitionend", function () {
+    setTimeout(() => {
+      flag = false;
+    }, 300);
+  });
 
-document.querySelector('#prev').addEventListener('click', function () {
-  
-  li[count -= 1].style.maxWidth = '0';
+
+
+
 
 });
