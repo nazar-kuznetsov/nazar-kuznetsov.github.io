186b1d0c7a272a937f0aaa638c2b38c1438d4ee5
Nazar Kuznetsov <nazar.kuznettsov@gmail.com>
2019-07-12 13:26:07 +0300

    yager

---
+++ yager/precache-manifest.308ce49df3df93f415517e9c6632e13b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d094ea28bf8a320c13a6390680b75ec1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac185ae015304b612db3",
+    "url": "/static/css/2.8781708f.chunk.css"
+  },
+  {
+    "revision": "a411a73fa93a5a6ddbf1",
+    "url": "/static/css/main.72fbe74e.chunk.css"
+  },
+  {
+    "revision": "ac185ae015304b612db3",
+    "url": "/static/js/2.edee5c4f.chunk.js"
+  },
+  {
+    "revision": "a411a73fa93a5a6ddbf1",
+    "url": "/static/js/main.67c5243a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6987bcc482500f459516dc0342836ee5",
+    "url": "/static/media/SFProDisplay-Regular.6987bcc4.ttf"
+  },
+  {
+    "revision": "89a0dda9a4e0fcbf9d90f507feb77486",
+    "url": "/static/media/SFProDisplay-Regular.89a0dda9.woff"
+  },
+  {
+    "revision": "ac1a5981ecc9db252eb4167d937d3114",
+    "url": "/static/media/SFProDisplay-Regular.ac1a5981.eot"
+  },
+  {
+    "revision": "47f0620f3790e38f621a9fbeb8eed066",
+    "url": "/static/media/SFUIText-Bold.47f0620f.ttf"
+  },
+  {
+    "revision": "bb08af6a35b24efe9f095224a0ab601f",
+    "url": "/static/media/SFUIText-Bold.bb08af6a.woff"
+  },
+  {
+    "revision": "bfadacfca40727188fe050019c6067f6",
+    "url": "/static/media/SFUIText-Bold.bfadacfc.eot"
+  },
+  {
+    "revision": "eeedcc9e7726aaac93a3296ad809260f",
+    "url": "/static/media/acept.eeedcc9e.pdf"
+  },
+  {
+    "revision": "c22c75450455e832b59f77db2e8b115c",
+    "url": "/static/media/banner.c22c7545.png"
+  },
+  {
+    "revision": "ba893a5f82d291eb0890ede33d0e20a4",
+    "url": "/static/media/offer1.ba893a5f.png"
+  },
+  {
+    "revision": "dd8bf7316425e202d0349a759fc9bf28",
+    "url": "/static/media/offer2.dd8bf731.png"
+  },
+  {
+    "revision": "53a8be20d803264ea74dec8aaa0ae0a2",
+    "url": "/static/media/offer3.53a8be20.png"
+  },
+  {
+    "revision": "22d2b93c00f675c71ab66e601871f8b3",
+    "url": "/static/media/pol.22d2b93c.pdf"
+  }
+]);
