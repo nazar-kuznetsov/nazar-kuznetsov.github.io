529d5dbfc38c838b4b1a2b5f95b666063f34a876
Nazar Kuznetsov <nazar.kuznettsov@gmail.com>
2019-08-12 20:01:53 +0300

    update

--- calc-money/precache-manifest.f274a8eb7c0abbc7ea9835cc79d2638e.js
+++ calc-money/precache-manifest.f274a8eb7c0abbc7ea9835cc79d2638e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e44c7b194d9c6c8f8fe5959c4db4c809",
+    "revision": "bd8268c4adaf3d8b846f72d5e68daaf7",
     "url": "./index.html"
   },
   {
-    "revision": "7b2155e392548936c4b2",
-    "url": "./static/css/main.8fb591bb.chunk.css"
+    "revision": "d0775ca1fb4ff755fe55",
+    "url": "./static/css/main.0ac74cfb.chunk.css"
   },
   {
     "revision": "41a57256f5a5da2c481a",
     "url": "./static/js/2.87046674.chunk.js"
   },
   {
-    "revision": "7b2155e392548936c4b2",
-    "url": "./static/js/main.71880d51.chunk.js"
+    "revision": "d0775ca1fb4ff755fe55",
+    "url": "./static/js/main.cd23f958.chunk.js"
   },
   {
     "revision": "623e122b18417ce78d20",
